7096dd583089042f7a4a61ad9b9adb7b7bb01b7f
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-04 02:02:12 -0500

    Update deploy_parameters.py

--- projects/simple_lambda-project/config/deploy_parameters.py
+++ projects/simple_lambda-project/config/deploy_parameters.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from simple_lambda.boto_ses import bsm
-from simple_lambda.config.init import config
+import aws_ops_alpha.api as aws_ops_alpha
+from simple_lambda.boto_ses import boto_ses_factory
+from simple_lambda.config.init import config, EnvEnum
 
-config.deploy(bsm=bsm, parameter_with_encryption=True)
-# config.delete(bsm=bsm, use_parameter_store=True)
+bsm_collection = {
+    "all": boto_ses_factory.bsm_devops,
+    EnvEnum.sbx.value: boto_ses_factory.bsm_sbx,
+    EnvEnum.tst.value: boto_ses_factory.bsm_tst,
+    EnvEnum.prd.value: boto_ses_factory.bsm_prd,
+}
+config.deploy(bsm=bsm_collection, parameter_with_encryption=True)
+# config.delete(bsm=bsm_collection, use_parameter_store=True)
