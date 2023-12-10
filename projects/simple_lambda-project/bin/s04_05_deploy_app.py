cc980318933ee5f6cf2ac253498ac9dcb2c8a95f
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-10 10:42:59 -0500

    c

--- projects/simple_lambda-project/bin/s04_05_deploy_app.py
+++ projects/simple_lambda-project/bin/s04_05_deploy_app.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from simple_lambda.ops.deploy import deploy_app
+from simple_lambda.ops import deploy_app
 
 deploy_app(check=True)
-
-# from automation.deploy import deploy_app
-#
-# from simple_lambda.paths import dir_cdk
-# from simple_lambda.boto_ses import bsm
-# from simple_lambda.config.init import EnvEnum, config
-#
-#
-# deploy_app(
-#     bsm=bsm,
-#     config=config,
-#     s3dir_lambda=config.env.s3dir_lambda,
-#     tags=config.env.aws_tags,
-#     dir_cdk=dir_cdk,
-#     stack_name=config.env.cloudformation_stack_name,
-#     prod_env_name=EnvEnum.prd.value,
-#     env_name=config.env.env_name,
-#     check=True,
-# )
--- projects/simple_lambda-project/bin/s04_06_delete_app.py
+++ projects/simple_lambda-project/bin/s04_06_delete_app.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from automation.deploy import delete_app
+from simple_lambda.ops import delete_app
 
-from simple_lambda.paths import dir_cdk
-from simple_lambda.boto_ses import bsm
-from simple_lambda.config.init import EnvEnum, config
-
-
-delete_app(
-    bsm=bsm,
-    dir_cdk=dir_cdk,
-    stack_name=config.env.cloudformation_stack_name,
-    prod_env_name=EnvEnum.prd.value,
-    env_name=config.env.env_name,
-    check=True,
-)
+delete_app(check=True)
