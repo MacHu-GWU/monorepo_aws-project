536bfbdb4af9ddb5e2067e2b2728eb215f41f54b
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-10 12:36:00 -0500

    Update s03_01_run_unit_test.py

--- projects/simple_lambda-project/bin/s03_01_run_unit_test.py
+++ projects/simple_lambda-project/bin/s03_01_run_unit_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from simple_lambda.boto_ses import boto_ses_factory
+print(boto_ses_factory.workload_bsm_list)
+
 from simple_lambda.ops import build_lambda_source, run_unit_test
 
 build_lambda_source(verbose=False)
