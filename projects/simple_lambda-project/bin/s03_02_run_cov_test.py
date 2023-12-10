7c93f7c65af6be790b8c254f3b8f7a0d8458225e
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-10 12:42:26 -0500

    Update s03_02_run_cov_test.py

--- projects/simple_lambda-project/bin/s03_02_run_cov_test.py
+++ projects/simple_lambda-project/bin/s03_02_run_cov_test.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from simple_lambda.boto_ses import boto_ses_factory
-print(boto_ses_factory.bsm_devops.aws_account_id)
+print(boto_ses_factory.bsm_devops.aws_account_id[:4])
 for bsm in boto_ses_factory.workload_bsm_list:
-    print(bsm.aws_account_id)
+    print(bsm.aws_account_id[:4])
 from simple_lambda.ops import build_lambda_source, run_cov_test
 
 build_lambda_source(verbose=False)
