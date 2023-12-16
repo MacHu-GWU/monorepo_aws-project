6d8037a2aa0af25fd452104652c54d3f2b5e871f
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-15 23:41:16 -0500

    c

--- projects/simple_cdk1-project/bin/s03_02_run_cov_test.py
+++ projects/simple_cdk1-project/bin/s03_02_run_cov_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from simple_cdk1.ops import build_lambda_source, run_cov_test
+from simple_cdk1.ops import run_cov_test
 
-build_lambda_source(verbose=False)
 run_cov_test(check=True)
--- projects/simple_cdk1-project/bin/s03_04_run_int_test.py
+++ projects/simple_cdk1-project/bin/s03_04_run_int_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from simple_cdk1.ops import build_lambda_source, run_int_test
+from simple_cdk1.ops import run_int_test
 
-build_lambda_source(verbose=False)
 run_int_test(check=True)
