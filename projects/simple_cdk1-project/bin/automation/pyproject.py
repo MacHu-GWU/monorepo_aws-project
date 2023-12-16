699743fd15220b2d74ef1cb3c8045aac048348b8
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-15 23:45:27 -0500

    c

--- projects/simple_cdk1-project/bin/automation/pyproject.py
+++ projects/simple_cdk1-project/bin/automation/pyproject.py
@@ -4,10 +4,7 @@
 Parse the ``pyproject.toml`` file.
 """
 
-from pathlib_mate import Path
 from pyproject_ops.api import PyProjectOps
-
-dir_project_root = Path.dir_here(__file__).parent.parent
-path_pyproject_toml = dir_project_root / "pyproject.toml"
+from .paths import path_pyproject_toml
 
 pyproject_ops = PyProjectOps.from_pyproject_toml(path_pyproject_toml)
