c7e7b19b178907563bb06b2ac35821e62df50ff9
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-15 23:39:48 -0500

    Update deploy_parameters.py

--- projects/simple_cdk1-project/config/deploy_parameters.py
+++ projects/simple_cdk1-project/config/deploy_parameters.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from simple_cdk1.boto_ses import boto_ses_factory
-from simple_cdk1.config.api import config, EnvNameEnum
+from simple_cdk1.ops import deploy_config, delete_config
 
-bsm_collection = {
-    "all": boto_ses_factory.bsm_devops,
-    EnvNameEnum.devops.value: boto_ses_factory.bsm_devops,
-    EnvNameEnum.sbx.value: boto_ses_factory.bsm_sbx,
-    EnvNameEnum.tst.value: boto_ses_factory.bsm_tst,
-    EnvNameEnum.prd.value: boto_ses_factory.bsm_prd,
-}
-config.deploy(bsm=bsm_collection, parameter_with_encryption=True)
-# config.delete(bsm=bsm_collection, use_parameter_store=True)
+deploy_config(check=False)
+# delete_config(check=False)
