321641a1b81d22ac77f43378c21cc011b10e00fe
Sanhe <MacHu-GWU@users.noreply.github.com>
2024-02-07 14:24:20 -0500

    c

---
+++ projects/simple_glue-project/bin/s04_04_lambda_build_layer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from simple_glue.ops import publish_lambda_layer
+
+publish_lambda_layer()
---
+++ projects/simple_glue-project/lambda_app/lambda_function.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from simple_glue.lbd.hello import lambda_handler as hello_handler
+from simple_glue.lbd.s3sync import lambda_handler as s3sync_handler
