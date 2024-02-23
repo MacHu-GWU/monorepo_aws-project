79ba2af54e8210585fd7dbd3f9f09f56fed23ece
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-02-22 21:51:29 -0500

    chore

--- bootstrap/python_lib/run_step.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import subprocess
-from .paths import bin_venv_python, path_cli
-
-
-def run_step(step_name: str):
-    subprocess.run([f"{bin_venv_python}", f"{path_cli}", step_name])
--- bootstrap/run_bootstrap.py
+++ bootstrap/run_bootstrap.py
@@ -17,26 +17,30 @@ workload_bsm_list = [
     bsm_prd,
 ]
 
-github_stack_name = "monorepo-aws-github-oidc"
+project_name = "monorepo_aws"
+project_name_slug = project_name.replace("_", "-")
+project_name_snake = project_name.replace("-", "_")
+
+github_stack_name = f"{project_name_slug}-github-oidc"
 github_org = "MacHu-GWU"
-github_repo = "monorepo_aws-project"
-gh_action_role_name = "monorepo-aws-github-oidc"
+github_repo = f"{project_name}-project"
+gh_action_role_name = f"{project_name_slug}-github-oidc"
 # use this if it is first time to setup GitHub OIDC connection in your AWS Account
 # oidc_provider_arn = ""
 # use this if it is NOT first time to setup GitHub OIDC connection in your AWS Account
 oidc_provider_arn = f"arn:aws:iam::{bsm_devops.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
 
-sbx_res_name = f"monorepo_aws-sbx-deployer-{bsm_sbx.aws_region}"
-tst_res_name = f"monorepo_aws-tst-deployer-{bsm_sbx.aws_region}"
-prd_res_name = f"monorepo_aws-prd-deployer-{bsm_sbx.aws_region}"
+sbx_res_name = f"{project_name_snake}-sbx-deployer-{bsm_sbx.aws_region}"
+tst_res_name = f"{project_name_snake}-tst-deployer-{bsm_sbx.aws_region}"
+prd_res_name = f"{project_name_snake}-prd-deployer-{bsm_sbx.aws_region}"
 
 workload_account_iam_policy_document = {
     "Version": "2012-10-17",
     "Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}],
 }
 
-devops_stack_name = "monorepo-aws-devops-deployer-us-east-1"
-devops_policy_name = "monorepo_aws-devops-us-east-1"
+devops_stack_name = f"{project_name_slug}-devops-deployer-us-east-1"
+devops_policy_name = f"{project_name_snake}-devops-us-east-1"
 
 artifacts_s3_bucket = "bmt-app-devops-us-east-1-artifacts"
 docs_s3_bucket = "bmt-app-devops-us-east-1-doc-host"
@@ -103,8 +107,8 @@ def run_setup_devops_account_s3_bucket():
         artifacts_s3_bucket=artifacts_s3_bucket,
         docs_s3_bucket=docs_s3_bucket,
         workload_account_iam_permission_setup_list=workload_account_iam_permission_setup_list,
-        artifacts_s3_prefix="projects/monorepo_aws/",
-        docs_s3_prefix="projects/monorepo_aws/",
+        artifacts_s3_prefix=f"projects/{project_name}/",
+        docs_s3_prefix=f"projects/{project_name}/",
         white_list_your_ip=True,
     )
 
